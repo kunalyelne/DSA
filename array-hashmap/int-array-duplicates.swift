f39959915c57d80a7cc09451c6dcc8970ab3a276
Kunal <kunal17058@gmail.com>
2025-09-13 10:06:30 +0530

    DSA Day-1

---
+++ array-hashmap/int-array-duplicates.swift
@@ -0,0 +1,14 @@
+import Foundation
+func containsDuplicate(_ nums:[Int]) -> Bool {
+    var seen = Set<Int>()
+    for num in nums {
+        if seen.contains(num) {
+            return true
+        }
+        seen.insert(num)
+    }
+    return false
+}
+
+print(containsDuplicate([1,2,3,4]))
+print(containsDuplicate([1,2,3,4,1]))
