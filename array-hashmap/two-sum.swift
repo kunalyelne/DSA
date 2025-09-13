29e9c5ddef03026311866cc30e37420d965ad4c1
Kunal <kunal17058@gmail.com>
2025-09-13 10:37:23 +0530

    DSA: Day-2

---
+++ array-hashmap/two-sum.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+    var compliment = [Int: Int]()
+
+    for i in 0..<nums.count {
+        if let comp = compliment[nums[i]] {
+            return [comp, i]
+        }
+
+        compliment[target - nums[i]] = i
+    }
+
+    return []
+}
+
+print(twoSum([2, 7, 11, 15], 9))
+print(twoSum([3, 2, 4], 6))
