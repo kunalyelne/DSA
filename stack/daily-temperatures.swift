836f9f0f0f7a16ed3668b22ec28f63255379d509
Kunal <kunal17058@gmail.com>
2025-09-19 14:42:37 +0530

    DSA: Day-7

--- stack/daily-temperatures.swift
+++ stack/daily-temperatures.swift
@@ -19,3 +19,6 @@ func dailyTemperatures(_ temperatures: [Int]) -> [Int] {
 
     return res
 }
+
+print(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]))  // [1,1,4,2,1,1,0,0]
+print(dailyTemperatures([30, 40, 50, 60]))  // [1,1,1,0]
